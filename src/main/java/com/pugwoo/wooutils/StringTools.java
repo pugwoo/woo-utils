46f569768045c89baeb64a03afa81160f2062eb1
pugwoo <pugwoo@gmail.com>
2016-02-04 14:33:25 +0800

    金钱转中文大写

--- src/main/java/com/pugwoo/wooutils/StringTools.java
+++ src/main/java/com/pugwoo/wooutils/StringTools.java
@@ -22,4 +22,47 @@ public class StringTools {
     	return true;
     }
 	
+    /**
+     * 数字转换成中文大写钱币，精确到分，四舍五入；最大9万亿9。
+     */
+    public static String moneyToChineseUpper(double n) {
+        String UNIT = "万千佰拾亿千佰拾万千佰拾元角分";
+        String DIGIT = "零壹贰叁肆伍陆柒捌玖";
+        double MAX_VALUE = 9999999999999.99D;
+
+        if (n < 0 || n > MAX_VALUE) return "参数非法!";
+        long l = Math.round(n * 100);
+        if (l == 0) return "零元整";
+        String strValue = l + "";
+        // i用来控制数
+        int i = 0;
+        // j用来控制单位
+        int j = UNIT.length() - strValue.length();
+        StringBuilder rs = new StringBuilder();
+        boolean isZero = false;
+        for (; i < strValue.length(); i++, j++) {
+            char ch = strValue.charAt(i);
+
+            if (ch == '0') {
+                isZero = true;
+                if (UNIT.charAt(j) == '亿' || UNIT.charAt(j) == '万' || UNIT.charAt(j) == '元') {
+                    rs.append(UNIT.charAt(j));
+                    isZero = false;
+                }
+            } else {
+                if (isZero) {
+                    rs.append("零");
+                    isZero = false;
+                }
+                rs = rs.append(DIGIT.charAt(ch - '0')).append(UNIT.charAt(j));
+            }
+        }
+
+        if (!rs.toString().endsWith("分")) {
+            rs = rs.append("整");
+        }
+        String ret = rs.toString();
+        ret = ret.replaceAll("亿万", "亿");
+        return ret;
+    }
 }
