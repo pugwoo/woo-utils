3e43405d883b35b3f0b23361957a2b6053829ab2
pugwoo <pugwoo@gmail.com>
2017-07-17 21:24:46 +0800

    ===未完成

--- src/test/java/com/pugwoo/wooutils/redis/sync/TestSync.java
+++ src/test/java/com/pugwoo/wooutils/redis/sync/TestSync.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration(locations = {"classpath*:applicationContext-context.xml"})
+@ContextConfiguration(locations = {"classpath:applicationContext-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TestSync {
 
