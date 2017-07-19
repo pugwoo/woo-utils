d1986ccc7944923ca814957efe4c199a8ea8d847
pugwoo <pugwoo@gmail.com>
2017-07-19 10:51:55 +0800

    clean code

--- src/test/java/com/pugwoo/wooutils/redis/TransactionTest.java
+++ src/test/java/com/pugwoo/wooutils/redis/TransactionTest.java
@@ -2,16 +2,41 @@ package com.pugwoo.wooutils.redis;
 
 public class TransactionTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
 		final RedisHelper redisHelper = TestRedisHelper.getRedisHelper();
-		
-		String myNamespace = "MY-NAMESPACE";
-		
-		boolean lock = redisHelper.requireLock(myNamespace, "1234", 10);
-		System.out.println(lock);
-		
-		// 可以尝试把这个main程序跑2次，第一次可以拿到锁，第二次不行，要过10秒事务超时了，就又可以获得了。
+		final String nameSpace = "myname";
+        final String key = "key";
+        
+        for(int i=0;i<10;i++){
+            Thread thread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					// 同一时刻只有一个人可以拿到lock，返回true
+	                boolean requireLock = redisHelper.requireLock(nameSpace, key, 10);
+	                if(requireLock){
+	                    System.out.println(Thread.currentThread().getName() + "拿到锁");
+	                }else{
+	                    System.out.println(Thread.currentThread().getName() + "没有拿到锁，等待....");
+	                }
+	                if(!requireLock){
+	                    while (!requireLock){
+	                        requireLock = redisHelper.requireLock(nameSpace, key, 10);
+	                    }
+	                    System.out.println(Thread.currentThread().getName() + "等待后拿到锁"+System.currentTimeMillis());
+	                }
+	                try {
+	                    Thread.sleep(1000);
+	                } catch (InterruptedException e) {
+	                    e.printStackTrace();
+	                }
+	                redisHelper.releaseLock(nameSpace,key);
+
+	                System.out.println(Thread.currentThread().getName() + "释放锁");
+				}
+			});
+            thread.start();
+        }
 	}
 	
 }
