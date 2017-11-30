69b99129bce333196e82cb551f378feea4dc769e
pugwoo <pugwoo@gmail.com>
2017-11-30 12:38:34 +0800

    ListUtils add filter

--- src/main/java/com/pugwoo/wooutils/collect/ListUtils.java
+++ src/main/java/com/pugwoo/wooutils/collect/ListUtils.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ListUtils {
@@ -20,6 +21,19 @@ public class ListUtils {
 			return new ArrayList<>();
 		}
 		return list.stream().map(mapper).collect(Collectors.toList());
+	}
+	
+	/**
+	 * filter一个list
+	 * @param list
+	 * @param predicate
+	 * @return
+	 */
+	public static <T> List<T> filter(List<T> list, Predicate<? super T> predicate) {
+		if(list == null) {
+			return new ArrayList<>();
+		}
+		return list.stream().filter(predicate).collect(Collectors.toList());
 	}
 	
 	@SafeVarargs
