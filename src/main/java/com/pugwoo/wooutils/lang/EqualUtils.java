fd9938b1400a60fa76618ea0b977a91b8e657134
pugwoo <pugwoo@gmail.com>
2018-11-12 13:15:40 +0800

    release 0.5.1

--- src/main/java/com/pugwoo/wooutils/lang/EqualUtils.java
+++ src/main/java/com/pugwoo/wooutils/lang/EqualUtils.java
@@ -19,7 +19,8 @@ import java.util.stream.Stream;
  * 如果是Stream流类型，不比对，认为是相等的
  * 如果是用户类型，则往下继续比对
  *
- * 支持指定排除掉的属性，不区分大小写
+ * 支持最小化比对，只比对双方都有的属性(适用于不同的自定义类进行比对)
+ * 支持指定排除掉的属性，忽略属性不区分大小写
  * 如果有一个对象是null有一个不是，则认为不相等；如果两个都是null，则认为相等
  * 如果两个对象内存地址相同，则认为相等
  *
@@ -31,6 +32,7 @@ public class EqualUtils {
 
     private List<String> ignoreAttrs = new ArrayList<>();
     private boolean ignoreListOrder = false;
+    private boolean differentClassMinCompare = false; // 对于不同的类，启用最小化比对，只比对双方都有的属性
 
     /**
      * 忽略比对List元素的顺序
@@ -38,6 +40,14 @@ public class EqualUtils {
      */
     public EqualUtils ignoreListOrder(boolean ignore) {
         this.ignoreListOrder = ignore;
+        return this;
+    }
+
+    /**
+     * 对于不同的类，启用最小化比对，只比对双方都有的属性
+     */
+    public EqualUtils differentClassMinCompare(boolean isMinCompare) {
+        this.differentClassMinCompare = isMinCompare;
         return this;
     }
 
@@ -58,49 +68,90 @@ public class EqualUtils {
     }
 
     /**
+     * 重置忽略指定的属性，相当于不再忽略任何属性
+     * @return
+     */
+    public EqualUtils resetIgnoreAttr() {
+        ignoreAttrs.clear();
+        return this;
+    }
+
+    /**
      * 比对两个对象是否相等
      */
     public boolean isEqual(Object a, Object b) {
         if(a == b) {return true;}
         if(a == null || b == null) {return false;}
-        if(!a.getClass().equals(b.getClass())) {return false;}
-
-        if(a instanceof Comparable) {
-            return ((Comparable) a).compareTo(b) == 0;
-        }
-
-        if(a.getClass().isArray()) {
-            return compareArray(a, b);
-        }
-        if(a instanceof List) {
-            return compareList((List<?>) a, (List<?>) b);
-        }
-        if(a instanceof Map) {
-            return compareMap((Map<?, ?>) a, (Map<?, ?>) b);
-        }
-        if(a instanceof Set) {
-            return compareSet((Set<?>) a, (Set<?>) b);
-        }
-
-        if(a instanceof Stream || a instanceof InputStream || a instanceof OutputStream) {
+        if((a instanceof Stream || a instanceof InputStream || a instanceof OutputStream) &&
+                (b instanceof Stream || b instanceof InputStream || b instanceof OutputStream)) {
             return true;
         }
 
-        // 用户自定义对象，对于每个get方法的值进行比对
-        List<Method> methods = getAllGetterMethods(a.getClass());
-        try {
-            for(Method method : methods) {
-                Object aE = method.invoke(a);
-                Object bE = method.invoke(b);
-                if(!isEqual(aE, bE)) {
-                    return false;
-                }
-            }
-        } catch (Exception e) {
-            LOGGER.error("compare getter method fail", e);
+        // 不同枚举比对其toString值
+        if(a.getClass().isEnum() && b.getClass().isEnum() && !a.getClass().equals(b.getClass())) {
+            return Objects.equals(a.toString(), b.toString());
         }
 
-        return true;
+        if(a.getClass().equals(b.getClass())) {
+            if(a instanceof Comparable) {
+                return ((Comparable) a).compareTo(b) == 0;
+            }
+            if(a.getClass().isArray()) {
+                return compareArray(a, b);
+            }
+            if(a instanceof List) {
+                return compareList((List<?>) a, (List<?>) b);
+            }
+            if(a instanceof Map) {
+                return compareMap((Map<?, ?>) a, (Map<?, ?>) b);
+            }
+            if(a instanceof Set) {
+                return compareSet((Set<?>) a, (Set<?>) b);
+            }
+
+            // 用户自定义对象，对于每个get方法的值进行比对
+            List<Method> methods = getAllGetterMethods(a.getClass());
+            try {
+                for(Method method : methods) {
+                    Object aE = method.invoke(a);
+                    Object bE = method.invoke(b);
+                    if(!isEqual(aE, bE)) {
+                        return false;
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.error("compare getter method fail", e);
+                return false;
+            }
+            return true;
+        } else { // 对于不同类型的类，根据differentClassMinCompare的配置比对
+            List<Method> methods1 = getAllGetterMethods(a.getClass());
+            List<Method> methods2 = getAllGetterMethods(b.getClass());
+
+            try {
+                for(Method method1 : methods1) {
+                    boolean isMatchProperties = false;
+                    for(Method method2 : methods2) {
+                        if(method1.getName().equals(method2.getName())) {
+                            isMatchProperties = true;
+                            Object aE = method1.invoke(a);
+                            Object bE = method2.invoke(b);
+                            if(!isEqual(aE, bE)) {
+                                return false;
+                            }
+                            break;
+                        }
+                    }
+                    if(!differentClassMinCompare && !isMatchProperties) {
+                        return false;
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.error("compare getter method fail", e);
+                return false;
+            }
+            return true;
+        }
     }
 
     private boolean compareArray(Object a, Object b) {
@@ -208,21 +259,16 @@ public class EqualUtils {
      */
     private List<Method> getAllGetterMethods(Class<?> clazz) {
         List<Method> methods = new ArrayList<>();
-        do {
-            for (Method m : clazz.getMethods()) {
-                String methodName = m.getName();
-                if (methodName.startsWith("get") && m.getParameterTypes().length == 0
+        for (Method m : clazz.getMethods()) {
+            String methodName = m.getName();
+            if (methodName.startsWith("get") && m.getParameterTypes().length == 0
                     && Modifier.isPublic(m.getModifiers()) && !methodName.equals("getClass")) {
-
-                    String attName = methodName.substring(3).toLowerCase();
-                    if(!ignoreAttrs.contains(attName)) {
-                        methods.add(m);
-                    }
+                String attName = methodName.substring(3).toLowerCase();
+                if(!ignoreAttrs.contains(attName)) {
+                    methods.add(m);
                 }
             }
-            clazz = clazz.getSuperclass();
-        } while (clazz != null);
-
+        }
         return methods;
     }
 
