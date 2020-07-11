3f9e8a09e09cc5e6dae123e56ad46f587684dc2c
pugwoo <pugwoo@gmail.com>
2020-07-11 17:00:59 +0800

    benchmark result

--- src/test/java/com/pugwoo/wooutils/redis/benchmark/PerformanceBenchmark.java
+++ src/test/java/com/pugwoo/wooutils/redis/benchmark/PerformanceBenchmark.java
@@ -18,6 +18,7 @@ public class PerformanceBenchmark {
 
     /**
      * 家里server+笔记本: 300线程qps9049，200线程qps9000，100线程qps8488，50线程8303，10线程3893
+     * 台式+dev：300线程qps12262，200线程qps12503，100线程qps11550，50线程qps8771，10线程2444
      */
     @Test
     public void benchIncr() {
