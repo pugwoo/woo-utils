e2ff5b2bfbece9809bdfdd0da7729ea7706f3fc5
pugwoo <pugwoo@gmail.com>
2020-07-16 09:25:50 +0800

    TODO 增加查询消息队列状态的接口

--- src/main/java/com/pugwoo/wooutils/redis/RedisQueueStatus.java
+++ src/main/java/com/pugwoo/wooutils/redis/RedisQueueStatus.java
@@ -1,7 +1,57 @@
 package com.pugwoo.wooutils.redis;
 
+/**
+ * redis消息队列的状态
+ */
 public class RedisQueueStatus {
 
+    /**
+     * 消息队列中当前等待消费的消息数
+     */
+    private int pendingCount;
+
+    /**
+     * 消息队列中正在消费的消息数
+     */
+    private int doingCount;
+
+    /**
+     * 消息队列中当前所有消息的总数
+     */
+    private int totalCount;
+
+    /**
+     * 最后一个自然天的消费发送总数 （暂不实现）
+     */
+    // private int daySendMsgCount;
+
+    /**
+     * 最近一个自然天的消息消费超时数 （暂不实现）
+     */
+    // private int dayExpireMsgCount;
 
 
+    public int getPendingCount() {
+        return pendingCount;
+    }
+
+    public void setPendingCount(int pendingCount) {
+        this.pendingCount = pendingCount;
+    }
+
+    public int getDoingCount() {
+        return doingCount;
+    }
+
+    public void setDoingCount(int doingCount) {
+        this.doingCount = doingCount;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
 }
