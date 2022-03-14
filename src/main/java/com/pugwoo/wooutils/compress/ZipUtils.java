0a9a4f06a0aa2ce7e83200f3e2c036cb56aeb1e2
sapluk <yeah16@qq.com>
2022-03-14 22:36:22 +0800

    improve ZipUtils流的关闭及异常处理

--- src/main/java/com/pugwoo/wooutils/compress/ZipUtils.java
+++ src/main/java/com/pugwoo/wooutils/compress/ZipUtils.java
@@ -20,25 +20,37 @@ import java.util.zip.ZipOutputStream;
 public class ZipUtils {
     
     public static class ZipItem {
-        /**文件名，包含目录形式：用/隔开，例如a/b/c.txt*/
+        /** 文件名，包含目录形式：用/隔开，例如a/b/c.txt */
         public String fileName;
-        /**文件对应的输入流*/
+        /** 文件对应的输入流 */
         public InputStream in;
     }
     
-    public static class Iterator {
-        private ZipInputStream zin;
-        private Iterator(ZipInputStream zin) {
+    public interface Iterator {
+        /**
+         * 获得下一个ZipItem，当返回null则表示读取完
+         * @return zipItem or null
+         * @throws IOException 解压操作异常 包括读取流异常/字符集错误等
+         */
+        ZipItem next() throws IOException;
+    }
+    
+    public static class ZipInputStreamIterator implements Iterator {
+        private final ZipInputStream zin;
+        private ZipInputStreamIterator(ZipInputStream zin) {
             this.zin = zin;
         }
+        
         /**
-         * 获得下一个ZipItem，当返回null则表示读取完。【重要】ZipItem中的输入流in必须在本次读取完。
-         * @return
-         * @throws IOException
+         * 获得下一个ZipItem，当返回null则表示读取完。<br>
+         * 【重要】ZipItem中的输入流in必须在本次读取完。
+         * @return zipItem
+         * @throws IOException 解压操作异常 {@link ZipInputStream#getNextEntry()}
          */
+        @Override
         public ZipItem next() throws IOException {
             ZipEntry ze = zin.getNextEntry();
-            if(ze == null) {
+            if (ze == null) {
                 return null;
             }
             ZipItem zipItem = new ZipItem();
@@ -50,50 +62,66 @@ public class ZipUtils {
     
     /**
      * 压缩文件
-     * @param zipItems
-     * @param out 不会自动close
+     * @param zipItems zipItemList 全部zipItem.in会自动关闭
+     * @param out 会自动close
      */
     public static void zip(List<ZipItem> zipItems, OutputStream out) throws IOException {
-        ZipOutputStream zipOut = new ZipOutputStream(out);
-        if(zipItems != null) {
-            for(ZipItem zipItem : zipItems) {
-                zipOut.putNextEntry(new ZipEntry(zipItem.fileName));
-                int len = -1;
-                byte[] buff = new byte[4096];
-                while ((len = zipItem.in.read(buff)) != -1) {
-                    zipOut.write(buff, 0, len);
+        ZipOutputStream zipOut = null;
+        try {
+            zipOut = new ZipOutputStream(out);
+            if (zipItems != null) {
+                for (ZipItem zipItem : zipItems) {
+                    zipOut.putNextEntry(new ZipEntry(zipItem.fileName));
+                    IOUtils.copy(zipItem.in, zipOut);
                 }
-                zipItem.in.close();
             }
+        } finally {
+            if (zipItems != null) {
+                for (ZipItem zipItem : zipItems) {
+                    IOUtils.close(zipItem.in);
+                }
+            }
+            IOUtils.close(zipOut);
+            IOUtils.close(out);
         }
-        zipOut.close();
     }
     
     /**
      * 解压文件，节省内存方式
-     * @param in 不会自动close
-     * @return
+     * @param in 不会自动close，close之后不能再调用Iterator.next()，请在处理完压缩包内文件后关闭
+     * @return zipInputStreamIterator
      */
-    public static Iterator unzip(InputStream in) throws Exception {
+    public static ZipInputStreamIterator unzip(InputStream in) {
         ZipInputStream zin = new ZipInputStream(in);
-        return new Iterator(zin);
+        return new ZipInputStreamIterator(zin);
     }
     
     /**
      * 解压文件，会将文件解压到内存中，该方式会占用较多内存
-     * @param in
-     * @return
-     * @throws Exception
+     * @param in 不会自动close，调用后可以立即关闭，因为数据都加载到内存中了
+     * @return zipItemList 注意zipItem中的in要自行关闭
      */
-    public static List<ZipItem> unzipAll(InputStream in) throws Exception {
+    public static List<ZipItem> unzipAll(InputStream in) throws IOException {
         Iterator it = unzip(in);
-        ZipItem zipItem = null;
+        ZipItem zipItem;
         List<ZipItem> zipItems = new ArrayList<>();
-        while((zipItem = it.next()) != null) {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            IOUtils.copy(zipItem.in, out);
-            zipItem.in = new ByteArrayInputStream(out.toByteArray());
-            zipItems.add(zipItem);
+        try {
+            while ((zipItem = it.next()) != null) {
+                ByteArrayOutputStream out = null;
+                try {
+                    out = new ByteArrayOutputStream();
+                    IOUtils.copy(zipItem.in, out);
+                    zipItem.in = new ByteArrayInputStream(out.toByteArray());
+                    zipItems.add(zipItem);
+                } finally {
+                    IOUtils.close(out);
+                }
+            }
+        } catch (IOException ioException) {
+            for (ZipItem item : zipItems) {
+                IOUtils.close(item.in);
+            }
+            throw ioException;
         }
         return zipItems;
     }
