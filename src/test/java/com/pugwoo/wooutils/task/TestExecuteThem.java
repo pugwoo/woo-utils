1deabe9af2e6422184a09cb23b7ae8a4e8a554b2
pugwoo <pugwoo@gmail.com>
2022-09-26 22:45:40 +0800

    单元测试

--- src/test/java/com/pugwoo/wooutils/task/TestExecuteThem.java
+++ src/test/java/com/pugwoo/wooutils/task/TestExecuteThem.java
@@ -1,31 +1,36 @@
 package com.pugwoo.wooutils.task;
 
-import java.util.Date;
+import com.pugwoo.wooutils.lang.NumberUtils;
+import org.junit.Test;
+
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 public class TestExecuteThem {
 
-	public static void main(String[] args) {
+	@Test
+	public void testBasic() {
+		// 默认10个线程
 		ExecuteThem executeThem = new ExecuteThem();
-		
+
+		// 10个线程，100个任务，每个3秒，那么一共30秒可以执行完
 		for(int i = 0; i < 100; i++) {
 			final int fi = i;
-			executeThem.add(new Callable<Object>() {
-				@Override
-				public Object call() throws Exception {
-					Thread.sleep(3000);
-					return fi;
-				}
+			executeThem.add(() -> {
+				Thread.sleep(3000);
+				return fi;
 			});
 		}
-		
-		System.out.println("waiting:" + new Date());
+
+		long start = System.currentTimeMillis();
 		List<Object> results = executeThem.waitAllTerminate();
-		System.out.println("end:" + new Date());
-		for(Object result : results) {
-		    System.out.println(result);
-		}
+		long end = System.currentTimeMillis();
+
+		long cost = end - start;
+		assert cost > 30_000 && cost < 30_100;
+
+		BigDecimal sum = NumberUtils.sum(results);
+		assert sum.intValue() == 4950;
 	}
 	
 }
