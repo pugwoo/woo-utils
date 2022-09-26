f92dd6cb9e44bcc3f7624ca08de23d1122cb35bc
pugwoo <pugwoo@gmail.com>
2022-09-26 23:02:49 +0800

    单元测试

--- src/test/java/com/pugwoo/wooutils/task/TestExecuteThem.java
+++ src/test/java/com/pugwoo/wooutils/task/TestExecuteThem.java
@@ -32,5 +32,38 @@ public class TestExecuteThem {
 		BigDecimal sum = NumberUtils.sum(results);
 		assert sum.intValue() == 4950;
 	}
+
+	@Test
+	public void testBlock() {
+		// 默认10个线程，最长等待队列长度是5
+		ExecuteThem executeThem = new ExecuteThem(10, 5);
+
+		// 30个任务，每个3秒
+		// 添加了15个之后（10个执行5个等待中）等待3秒
+		// 会等待前10个执行完，然后再加10个，再等待3秒
+		// 最后再加5个，就完成了，一共等待了6秒
+		long start = System.currentTimeMillis();
+
+		for(int i = 0; i < 30; i++) {
+			final int fi = i;
+			executeThem.add(() -> {
+				Thread.sleep(3000);
+				return fi;
+			});
+		}
+
+		long end = System.currentTimeMillis();
+
+		long cost = end - start;
+		assert cost > 6000 && cost < 6050;
+
+		// 这个最后最后一批执行，3秒完成
+		start = System.currentTimeMillis();
+		executeThem.waitAllTerminate();
+		end = System.currentTimeMillis();
+		cost = end - start;
+		assert cost > 3000 && cost < 3020;
+
+	}
 	
 }
