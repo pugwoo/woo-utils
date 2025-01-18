ac0086e6a9d942e3107ca39b6a4cb93ac40263bf
pugwoo <pugwoo@gmail.com>
2025-01-18 22:12:06 +0800

    for test module

---
+++ for-test/src/test/java/com/pugwoo/for_test/ForTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pugwoo.for_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
